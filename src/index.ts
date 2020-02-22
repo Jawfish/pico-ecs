65226bdd394a2ffbac0c794b57fc6aba68ea36ca
Jawfish <james@jawfish.llc>
2020-02-22 15:40:29 -0500

    removed unnecessary index.ts content

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,1 @@
 import { EntityManager } from './lib/EntityManager';
-import { TestComponentOne, TestComponentTwo } from '../test/TestComponents';
-
-const testManager = new EntityManager();
-const testEntity = testManager.createEntity();
-testEntity.addComponent(new TestComponentOne(testEntity, 25, 35));
-console.log(testEntity.testComponentOne.x);
