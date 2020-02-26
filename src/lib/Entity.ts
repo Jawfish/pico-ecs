ee2918764a90cefb2a9a99da92a59521c3c0220e
James Fitzgerald <james@jawfish.llc>
2020-02-25 22:09:55 -0500

    added docs

--- src/lib/Entity.ts
+++ src/lib/Entity.ts
@@ -17,18 +17,33 @@ export class Entity {
         this.tags = [];
     }
 
+    /**
+     * @param {Object} component The component to add to this entity
+     * @returns {Entity} This entity
+     */
     addComponent = (component: Object): Entity => {
         this.manager.addComponent(this, component);
         return this;
     };
 
+    /**
+     * @param {string} component The component to remove rom this entity
+     * @returns {Entity} This entity
+     */
     removeComponent = (component: string): Entity => {
         this.manager.removeComponent(this, component);
         return this;
     };
 
+    /**
+     * Remove all components from this entity
+     */
     removeAllComponents = () => this.manager.removeAllComponents(this);
 
+    /**
+     * @param {string}  component The component to check this entity for
+     * @returns {boolean} True if this entity has the specified component
+     */
     hasComponent = (component: string): boolean => {
         let exists = false;
         for (let i = 0; i < this.components.length; i++) {
@@ -40,6 +55,10 @@ export class Entity {
         return exists;
     };
 
+    /**
+     * @param {string[]} componentsToCheck Array of compnents to check this entity for
+     * @returns {boolean} True if this entity has all specified components
+     */
     hasAllComponents = (componentsToCheck: string[]): boolean => {
         for (let i = 0; i < componentsToCheck.length; i++) {
             if (!this.hasComponent(componentsToCheck[i])) {
@@ -49,14 +68,28 @@ export class Entity {
         return true;
     };
 
-    hasTag = (tag: string) => this.tags.includes(tag);
+    /**
+     * @param {string} tag The tag to check this entity for
+     * @returns {boolean} True if this entity has the specified tag
+     */
+    hasTag = (tag: string): boolean => this.tags.includes(tag);
 
-    addTag = (tag: string) => {
+    /**
+     * @param {string} tag Tag to add to this entity
+     * @returns {Entity}
+     */
+    addTag = (tag: string): Entity => {
         this.manager.addTag(this, tag);
         return this;
     };
 
+    /**
+     * @param {string} tag The tag to remove from this entity
+     */
     removeTag = (tag: string) => this.manager.removeTag(this, tag);
 
+    /**
+     * Remove and garbage collect this entity instance
+     */
     remove = () => this.manager.removeEntity(this);
 }
--- src/lib/EntityManager.ts
+++ src/lib/EntityManager.ts
@@ -8,6 +8,10 @@ export class EntityManager {
         this.entities = [];
     }
 
+    /**
+     * Create an entity
+     * @returns {Entity} The created entity
+     */
     createEntity = (): Entity => {
         const entity: Entity = new Entity(this);
         this.entities.push(entity);
@@ -15,8 +19,16 @@ export class EntityManager {
         return entity;
     };
 
+    /**
+     * Get a list of all entity instances
+     * @returns {Entity[]} Array of entity instances
+     */
     listEntities = (): Entity[] => this.entities;
 
+    /**
+     * Remove entity instances that contain the specified tag
+     * @param {string} tag The tag to check entities for
+     */
     removeEntitiesByTag = (tag: string) => {
         const entities = this.tags[tag];
         entities.forEach((entity: Entity) => {
@@ -24,12 +36,19 @@ export class EntityManager {
         });
     };
 
+    /**
+     * Removes and garbage collect all existing entities
+     */
     removeAllEntities = () => {
         this.entities.forEach(entity => {
             entity.remove();
         });
     };
 
+    /**
+     * Remove and garbage collect the specified entity
+     * @param {Entity} entity The entity to remove
+     */
     removeEntity = (entity: Entity) => {
         if (!this.entities.includes(entity)) {
             throw new Error('Tried to remove nonexistent entity');
@@ -39,6 +58,11 @@ export class EntityManager {
         this.listEntities();
     };
 
+    /**
+     * Adds a tag to the specified entity
+     * @param {Entity} entity The entity to add the tag to
+     * @param {string} tag The tag to add to the entity
+     */
     addTag = (entity: Entity, tag: string) => {
         if (tag === '') {
             throw new Error('Tried to add an empty tag');
@@ -51,6 +75,11 @@ export class EntityManager {
         entity.tags.push(tag);
     };
 
+    /**
+     * Removes the specified tag from the entity
+     * @param {Entity} entity The entity to remove the tag from
+     * @param {string} tag The tag to remove from the entity
+     */
     removeTag = (entity: Entity, tag: string) => {
         const entities = this.tags[tag];
         if (!entities) return;
@@ -62,6 +91,10 @@ export class EntityManager {
         entity.tags.splice(entity.tags.indexOf(tag), 1);
     };
 
+    /**
+     * List all existing tags on this manager
+     * @returns {string[]} Array of all tags on this manager
+     */
     listTags = (): string[] => {
         const tagList = [];
         for (const tag of Object.keys(this.tags)) {
@@ -70,17 +103,31 @@ export class EntityManager {
         return tagList;
     };
 
+    /**
+     * Adds a component to the specified entity
+     * @param {Entity} entity The entity to add a component to
+     * @param {Object} component The component to add to the entity
+     */
     addComponent = (entity: Entity, component: Object) => {
         if (entity.components.includes(component)) return;
         entity[component.constructor.name] = component;
         entity.components.push(component);
     };
 
+    /**
+     * Remove all components from the specified entity
+     * @param {Entity} entity The entity to remove all components from
+     */
     removeAllComponents = (entity: Entity) =>
         entity.components.forEach(component =>
             entity.removeComponent(component.constructor.name)
         );
 
+    /**
+     * Remove the specified component from the specified entity
+     * @param {Entity} entity The entity from which to remove the component
+     * @param {string} component The component to remove from the entity
+     */
     removeComponent = (entity: Entity, component: string) => {
         if (!entity.hasComponent(component)) return;
         entity.components = entity.components.filter(
@@ -91,6 +138,11 @@ export class EntityManager {
         delete entity[component];
     };
 
+    /**
+     * Get a list of all entities that have the specified components
+     * @param {string[]} components The components to check entities for
+     * @returns {Entity[]} Array of entities that contain the specified components
+     */
     queryComponentOwners = (components: string[]): Entity[] => {
         const componentOwners: Entity[] = [];
         this.entities.forEach(entity => {
@@ -102,12 +154,21 @@ export class EntityManager {
         return componentOwners;
     };
 
+    /**
+     * Get a list of all entities that haev the specified tag
+     * @param {string} tag The tag to check entities for
+     * @returns {Entity[]} Array of entities that have the specified tag
+     */
     queryTagOwners = (tag: string): Entity[] => {
         let entities = this.tags[tag];
         if (entities === undefined) entities = this.tags[tag] = [];
         return entities;
     };
 
+    /**
+     * Count how many entities exist on this manager
+     * @returns {number} The number of entities that exist on this manager
+     */
     count = (): number => {
         return this.entities.length;
     };
